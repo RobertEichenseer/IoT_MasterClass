5cbad05cf652714a9ba23e6013fa8014be84fb95
Robert Eichenseer <roberteichenseer@live.de>
2015-03-18 17:30:30 +0100

    Command & Control UI added

---
+++ MC_CommandControlFrontEnd/Controllers/CommandController.cs
@@ -0,0 +1,85 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace MC_CommandControlFrontEnd.Controllers
+{
+    public class CommandController : ApiController
+    {
+        [HttpGet]
+        [Route("api/CommandControl/{deviceId}")]
+        public HttpResponseMessage Get(string deviceId)
+        {
+            if (String.IsNullOrEmpty(deviceId))
+                return new HttpResponseMessage(HttpStatusCode.NoContent);
+
+            ConfigurationOptions configurationOptions = new ConfigurationOptions();
+            configurationOptions.EndPoints.Add("<< add your Redis cache name here >>.redis.cache.windows.net");
+            configurationOptions.Ssl = true;
+            configurationOptions.Password = "<< Add Access Key Here >>";
+
+            ConnectionMultiplexer connectionMultiplexer = ConnectionMultiplexer.Connect(configurationOptions);
+            IDatabase commandQueue = connectionMultiplexer.GetDatabase();
+
+            RedisValue redisValue = commandQueue.ListRightPop(deviceId);
+            if (redisValue.HasValue)
+                return new HttpResponseMessage()
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Content = new StringContent(redisValue.ToString()),
+                };
+            else
+                return new HttpResponseMessage()
+                {
+                    StatusCode = HttpStatusCode.NoContent,
+                    Content = new StringContent(""),
+                };
+        }
+
+        [HttpPost]
+        [Route("api/CommandControl/{deviceId}/{command}")]
+        public async Task<HttpResponseMessage> Post(string deviceId, string command)
+        {
+            if (String.IsNullOrEmpty(deviceId) || String.IsNullOrEmpty(command))
+                return new HttpResponseMessage(HttpStatusCode.PartialContent);
+
+            ConfigurationOptions configurationOptions = new ConfigurationOptions();
+            configurationOptions.EndPoints.Add("<< add your Redis cache name here >>.redis.cache.windows.net");
+            configurationOptions.Ssl = true;
+            configurationOptions.Password = "<< Add Access Key Here >>";
+
+            ConnectionMultiplexer connectionMultiplexer = ConnectionMultiplexer.Connect(configurationOptions);
+            IDatabase commandQueue = connectionMultiplexer.GetDatabase();
+
+            await commandQueue.ListRightPushAsync(deviceId, command);
+
+            return new HttpResponseMessage(HttpStatusCode.Created);
+        }
+
+        [HttpDelete]
+        [Route("api/CommandControl/{deviceId}")]
+        public async Task<HttpResponseMessage> Delete(string deviceId)
+        {
+            if (String.IsNullOrEmpty(deviceId))
+                return new HttpResponseMessage(HttpStatusCode.PartialContent);
+
+            ConfigurationOptions configurationOptions = new ConfigurationOptions();
+            configurationOptions.EndPoints.Add("<< add your Redis cache name here >>.redis.cache.windows.net");
+            configurationOptions.Ssl = true;
+            configurationOptions.Password = "<< Add Access Key Here >>";
+
+            ConnectionMultiplexer connectionMultiplexer = ConnectionMultiplexer.Connect(configurationOptions);
+            IDatabase commandQueue = connectionMultiplexer.GetDatabase();
+
+            await commandQueue.KeyDeleteAsync(deviceId);
+            return new HttpResponseMessage(HttpStatusCode.OK);
+
+        }
+
+    }
+}
---
+++ MC_CommandControlFrontEnd/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MC_CommandControlFrontEnd.WebApiApplication" Language="C#" %>
